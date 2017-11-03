a94e44f75f73b5681803a6c0d442d99080a7c75d
Sean Crawford <Sheath51@gmail.com>
2017-11-03 02:34:44 +0000

    fixed some bugs and made the page auto refresh after adding to the list

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,48 @@
 var app = window.angular.module('app', [])
 
+app.factory('taskFetcher', taskFetcher)
 app.controller('mainCtrl', mainCtrl)
 
-function mainCtrl($scope) {
+function taskFetcher($http) {
 
-  $scope.taskList = [];
+    var API_ROOT = '/taskList'
+    return {
+        get: function () {
+            return $http
+                .get(API_ROOT)
+                .then(function (resp) {
+                    return resp.data
+                })
+        }
+    }
+}
 
-  $scope.addTask = function () {
-   
-  }
+function mainCtrl($scope, taskFetcher, $http) {
+
+    $scope.taskList = [];
+
+    taskFetcher.get()
+        .then(function (data) {
+            console.log(data);
+            $scope.taskList = data;
+        })
+
+    $scope.reload = function() {
+        location.reload();
+    }
+
+    $scope.addTask = function () {
+        var formData = $scope.Task;
+        console.log(formData);
+        var taskUrl = 'taskList';
+        $http({
+            url: taskUrl,
+            method: "POST",
+            data: formData
+        }).success(function (data, status, headers, config) {
+            console.log("Post Worked");
+        }).error(function (data, status, headers, config) {
+            console.log("Post Failed");
+        });
+    }
 }
