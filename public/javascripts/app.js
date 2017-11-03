c0b39edcdee63201c617b38bdb991fe6db3debcb
Sean Crawford <Sheath51@gmail.com>
2017-11-03 01:56:04 +0000

    more minor changes

---
+++ public/javascripts/app.js
@@ -0,0 +1,12 @@
+var app = window.angular.module('app', [])
+
+app.controller('mainCtrl', mainCtrl)
+
+function mainCtrl($scope) {
+
+  $scope.taskList = [];
+
+  $scope.addTask = function () {
+   
+  }
+}
